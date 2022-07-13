a912aa8935b5289b36d0274646f28e0b55c233bb
river <rrivverr@protonmail.com>
2022-07-12 23:47:07 -0400

    Add form for reservations

--- src/contact.js
+++ src/contact.js
@@ -2,10 +2,28 @@ import { element } from './utilities.js';
 
 const buildContact = () => {
 
-    console.log('contact page')
-    const page = element('div');
-    page.textContent = "TEST TEST TEST";
+    const formContainer = element('div', {"id": "form-container"});
+    const form = element('form', {"id": "reservation-form"});
 
+    const nameLabel = element('label', {"for": "name", "class": "form-label"});
+    nameLabel.textContent = "Full Name:"
+    const name = element('input', {"type": "text", "class": "form-input", "name": "name", "id": "form_name", "placeholder": "John Smith" })
+    nameLabel.appendChild(name);
+
+    const dateLabel = element('label', {"for": "date", "class": "form-label"});
+    dateLabel.textContent = "Date:";
+    const date = element("input", {"type": "date", "class": "form-input", "name": "name", "id": "form_date",});
+    dateLabel.appendChild(date);
+
+
+
+    form.appendChild(nameLabel);
+    form.appendChild(dateLabel);
+
+    console.log('contact page')
+    const page = element('div', {"class": "page"})
+    formContainer.appendChild(form);
+    page.appendChild(formContainer);
 
     return page;
 }
