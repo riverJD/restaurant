610fcd377b140b9fb1826c2cd5dc2232fbbcb745
river <rrivverr@protonmail.com>
2022-07-13 14:15:08 -0400

    Finalize welcome page

--- src/main.js
+++ src/main.js
@@ -14,33 +14,45 @@ const buildMain = () => {
     pageHeader.textContent = "welcome to bluebird"
 
     // Create a section of content, with text and image (optional);    
-    const createContent = (name, content, photo, first) => {
+    // Elements will be dynamically clased and id based on name.  
+    // content is a source of text content for the section
+    // firstElement is string name of element which will be placed to the left
+    const createContent = (name, content, photo, firstElement) => {
 
         const container = element('div', {"class": "page-content", 'id': `${name}-content`});
         const text = element('div', {"class": "text-content content", "id": `${name}-text`});
         text.textContent = content;
 
 
+        container.appendChild(text);
 
         if (photo != null){
             const picture = element('img', {"class": "photo", "id": `${name}-photo`, "src": photo})
+            
+            if (firstElement === 'photo'){
+                container.insertBefore(picture, text);
+            }
+            else {
             container.appendChild(picture);
+            }
+
+
         }
         else{
             container.classList.add("no-photo");
+            
         }
         
-        if (first === 'text'){
-            console.log(first);
-            text.classList.add('first')
-        }
-        container.appendChild(text);
+      
+
+    
+        //container.appendChild(text);
         return container;
     }
 
     const welcome = createContent("welcome", welcomeTextData);
     const table = createContent("table", "Our space is very limited, and reservations are required. Please call ahead if you have any specific dietary restrictions.", tableDData)
-    const farm = createContent("farm",farmTextData, birdData, "text");
+    const farm = createContent("farm",farmTextData, birdData, "photo");
     // farm contents
     /* const farmContent = element('div', {"class": 'page-content', "id": "farm-content"});
     const farm = element('img', {"class": "photo", "id": "farm-photo", "src": birdData});
